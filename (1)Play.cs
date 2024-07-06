9b3bfc229cd732b98d9e1da8deee68917f3a0862
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2024-07-06 15:21:23 +0300

    add file

---
+++ (1)Play.cs
@@ -0,0 +1,60 @@
+﻿class Play : IDisposable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+
+    bool disposed = false;
+
+    public Play(string title, string author, string genre, int year)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Genre: {Genre}");
+        Console.WriteLine($"Year: {Year}\n");
+    }
+
+    void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Deleting play.");
+            }
+            disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    ~Play()
+    {
+        Console.WriteLine($"Play {Title} has being destroyed.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Play play1 = new Play("PlayTitle1", "PlayAuthor1", "PlayGenre1", 2024);
+        Play play2 = new Play("PlayTitle2", "PlayAuthor2", "PlayGenre2", 2023);
+
+        play1.DisplayInfo();
+        play2.DisplayInfo();
+        play2.Dispose();
+    }
+}
---
+++ (2)Store.cs
@@ -0,0 +1,56 @@
+﻿public class Store : IDisposable
+{
+    public string StoreName { get; set; }
+    public string StoreAddress { get; set; }
+    public string StoreType { get; set; }
+    private bool disposed = false;
+
+    public Store(string storeName, string storeAddress, string storeType)
+    {
+        StoreName = storeName;
+        StoreAddress = storeAddress;
+        StoreType = storeType;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Store Name: {StoreName}");
+        Console.WriteLine($"Store Address: {StoreAddress}");
+        Console.WriteLine($"Store Type: {StoreType}");
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Closing store");
+            }
+            disposed = true;
+        }
+    }
+
+    ~Store()
+    {
+        Dispose(false);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Store store = new Store("Good Store", "123 Main St", "Grocery");
+        store.DisplayInfo();
+
+        Store anotherStore = new Store("Better Store", "456 High St", "Clothing");
+        anotherStore.DisplayInfo();
+        anotherStore.Dispose();
+    }
+}
