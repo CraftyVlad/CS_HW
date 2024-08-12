8d099228ba0a3d922042fe88b8999029b618fcc2
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2024-08-12 20:04:26 +0300

    added file

---
+++ Fractions.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+
+public class Fraction
+{
+
+    public int Numerator { get; set; }
+    public int Denominator { get; set; }
+
+    public Fraction()
+    {
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+
+    public override string ToString()
+    {
+        return $"{Numerator}/{Denominator}";
+    }
+}
+
+class Fractions
+{
+    static void Main()
+
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        List<Fraction> fractions = new List<Fraction>();
+
+        Console.WriteLine("Введіть кількість дробів:");
+        int n = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Введіть чисельник для дробу {i + 1}:");
+            int numerator = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"Введіть знаменник для дробу {i + 1}:");
+            int denominator = int.Parse(Console.ReadLine()!);
+
+            fractions.Add(new Fraction(numerator, denominator));
+        }
+
+        string jsonString = JsonSerializer.Serialize(fractions);
+        Console.WriteLine("Серіалізований масив дробів:");
+        Console.WriteLine(jsonString);
+
+        File.WriteAllText("../../../fractions.json", jsonString);
+        Console.WriteLine("Масив збережено у файл fractions.json.");
+
+        string loadedJsonString = File.ReadAllText("../../../fractions.json");
+        List<Fraction> loadedFractions = JsonSerializer.Deserialize<List<Fraction>>(loadedJsonString)!;
+
+        Console.WriteLine("Завантажений та десеріалізований масив дробів:");
+        foreach (var fraction in loadedFractions)
+        {
+            Console.WriteLine(fraction);
+        }
+    }
+}
---
+++ Magazine.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+
+public class Magazine
+{
+
+    public string Title {  get; set; }
+    public string Publisher { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public int PageCount { get; set; }
+
+    public override string ToString()
+    {
+        return $"Назва: {Title}, Видатництво: {Publisher}, Дата видання: {PublicationDate}, Кількість сторінок: {PageCount}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть назву журналу:");
+        string title = Console.ReadLine()!;
+
+        Console.WriteLine("Введіть назву видавця:");
+        string publisher = Console.ReadLine()!;
+
+        Console.WriteLine("Введіть дату видання (yyyy-mm-dd):");
+        DateTime publicationDate = DateTime.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Введіть кількість сторінок:");
+        int pageCount = int.Parse(Console.ReadLine()!);
+
+        Magazine magazine = new Magazine
+        {
+            Title = title,
+            Publisher = publisher,
+            PublicationDate = publicationDate,
+            PageCount = pageCount
+        };
+
+        Console.WriteLine("Інформвція про журнал:");
+        Console.WriteLine(magazine);
+
+        string jsonString = JsonSerializer.Serialize(magazine);
+        File.WriteAllText("../../../magazine.json", jsonString);
+        Console.WriteLine("Інформація про журнал збережена у файл 'magazine.json'.");
+
+        string loadedJsonString = File.ReadAllText("../../../magazine.json");
+        Magazine loadedMagazine = JsonSerializer.Deserialize< Magazine>(loadedJsonString)!;
+
+        Console.WriteLine($"Завантажена інформація про журнал: '{loadedMagazine}'.");
+    }
+}
