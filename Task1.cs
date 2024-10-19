98dbf7288ea7ae3cf7ac87cdbcd8bde0f9d28f87
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2024-10-19 12:43:48 +0300

    Update Task1.cs

--- Task1.cs
+++ Task1.cs
@@ -1,28 +1,28 @@
-﻿//using System.Net;
-//using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.NetworkInformation;
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        string hostname = Dns.GetHostName();
-//        Console.WriteLine($"Host name: {hostname}");
+class Program
+{
+    static void Main()
+    {
+        string hostname = Dns.GetHostName();
+        Console.WriteLine($"Host name: {hostname}");
 
-//        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+        NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
 
-//        foreach ( NetworkInterface netInterface in interfaces )
-//        {
-//            Console.WriteLine($"\nNet netInterface: {netInterface.Name}\nMAC-Address: {netInterface.GetPhysicalAddress()}");
+        foreach (NetworkInterface netInterface in interfaces)
+        {
+            Console.WriteLine($"\nNet netInterface: {netInterface.Name}\nMAC-Address: {netInterface.GetPhysicalAddress()}");
 
-//            IPInterfaceProperties properties = netInterface.GetIPProperties();
-//            foreach (UnicastIPAddressInformation ipInfo in properties.UnicastAddresses)
-//            {
-//                if (ipInfo.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-//                {
-//                    Console.WriteLine($"IP-Address: {ipInfo.Address}");
-//                }
-//            }
-//            Console.WriteLine($"Status: {netInterface.OperationalStatus}");
-//        }
-//    }
-//}
+            IPInterfaceProperties properties = netInterface.GetIPProperties();
+            foreach (UnicastIPAddressInformation ipInfo in properties.UnicastAddresses)
+            {
+                if (ipInfo.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine($"IP-Address: {ipInfo.Address}");
+                }
+            }
+            Console.WriteLine($"Status: {netInterface.OperationalStatus}");
+        }
+    }
+}
