bf16a04136a2dc98cd701d2b828313cfbd76d263
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2024-10-19 12:05:53 +0300

    completed tasks

---
+++ Task2.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter URL:");
+        string input = Console.ReadLine()!;
+
+        try
+        {
+            Uri uri = new Uri(input);
+
+            Console.WriteLine($"\nScheme: {uri.Scheme}\nHost: {uri.Host}\nPort: {uri.Port}\nPath: {uri.AbsolutePath}\nQuery: {uri.Query}\nFragment: {uri.Fragment}\nAbsolute address: {(uri.IsAbsoluteUri ? "Yes" : "No")}");
+        }
+        catch (UriFormatException)
+        {
+            Console.WriteLine("Invalid URL. Try again");
+        }
+    }
+}
